528a2ae5b0953373f3164410ebd118a8650bff98
Pawwz <pawwzietek@gmail.com>
2025-06-10 03:47:49 +0200

    Created database models

--- habits/Core/Habit.py
+++ habits/Core/Habit.py
@@ -7,5 +7,5 @@ class Habit:
     habit_id: int
     name: str
     user_id: int
-    activity_value_type: str  # 'int' or 'float'
+    activity_value_type: str  # 'int' or 'float' atm
     rep_obj_id: Optional[int] = field(default=None, compare=False)
--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    # id field is generated automatically
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=128)
+    email = models.EmailField()
+    recovery_code = models.CharField(max_length=20)
+
+
+class Habit(models.Model):
+
+    SUPPORTED_ACTIVITY_TYPES = {
+        "int": "Integer",
+        "float": "floating"
+    }
+
+    name = models.CharField(max_length=32)
+    description = models.TextField(default="Placeholder. Edit habit to set custom description")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_value_type = models.CharField(max_length=8, choices=SUPPORTED_ACTIVITY_TYPES)
+
+
+class FloatMeasurableActivity(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    value = models.FloatField()
+    date = models.DateField()
+
+
+class CountableActivity(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    date = models.DateField()
+
