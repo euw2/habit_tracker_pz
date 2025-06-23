81d08c902d146e57b6bb1b98e42c99f61dbd19bb
Pawwz <pawwzietek@gmail.com>
2025-06-23 17:05:03 +0200

    - added wait_for_db command to container start up

--- PZHabitTracker/settings.py
+++ PZHabitTracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'habits.apps.HabitsConfig',
+    'wait_for_db',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
