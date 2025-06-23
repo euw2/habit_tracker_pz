7ec1c0c34cc7044ff4c931d6cd28e49fcb2492fe
Pawwz <pawwzietek@gmail.com>
2025-06-23 16:41:24 +0200

    Added postgres database to docker Added script to generate development environment

--- env/dev_env_entry_point.sh
+++ env/dev_env_entry_point.sh
@@ -1,3 +1,3 @@
-# This file is an entry 1point for development environment
+# This file is an entry point for development environment
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
