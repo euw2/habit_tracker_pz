d486ae56c1ba61d3ac5dcc999497f57b143b4996
Pawwz85 <126493682+Pawwz85@users.noreply.github.com>
2025-07-09 22:35:06 +0200

    App secret key is now loaded from env variables

--- PZHabitTracker/settings.py
+++ PZHabitTracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q!bjic(d_*+dhrhu$c3sh(qq+#730jq_cd(zzu$z_%ui0k57il'
+SECRET_KEY = environ.get('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
